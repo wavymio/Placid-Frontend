7c2e29ef3a84851bb00fabf97a13f6175d24e9d2
michaelmio333 <israeloahimire@gmail.com>
2024-10-07 22:21:05 +0100

    updated room and synced video

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -196,6 +196,13 @@ const ChatBox = ({ room, loggedInUser, socket, scrollToTop }) => {
         resetInputs()
     }
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            console.log("yse")
+            handleSubmit(event)
+        }
+    }
+
     useEffect(() => {
         if (sendingQueue.length > 0 && !isSending) {
             sendNextMessage()
@@ -326,7 +333,7 @@ const ChatBox = ({ room, loggedInUser, socket, scrollToTop }) => {
                     const containerRect = container.getBoundingClientRect()
                     
                     // if the message is visible, scroll to the bottom 
-                    if (top >= containerRect.top && bottom <= containerRect.bottom) {
+                    if (top >= containerRect.top && bottom <= containerRect.bottom+25) {
                         scrollToMessage(lastMessageIndex)
                     }
                 }
@@ -380,8 +387,8 @@ const ChatBox = ({ room, loggedInUser, socket, scrollToTop }) => {
                             key={index} 
                             className={`text-xs sm:text-sm flex items-end ${message?.senderId?._id === loggedInUser._id ? 'justify-end' : null} gap-3`}>
                                 {((message?.senderId?._id !== loggedInUser._id) && (message?.senderId?._id !== messages[(index + 1)]?.senderId?._id)) ? (
-                                    <div className={`max-h-10 max-w-10 xs:max-h-12 xs:max-w-12 p-1 rounded-full border ${getBorderColour(message?.senderId?._id)}`}>
-                                        <img src={message.senderId.profilePicture} className='object-cover rounded-full ' />
+                                    <div className={`h-10 w-10 xs:h-12 xs:w-12 p-1 rounded-full border ${getBorderColour(message?.senderId?._id)}`}>
+                                        <img src={message.senderId.profilePicture} className='h-full w-full object-cover rounded-full ' />
                                     </div>
                                 ) : (
                                     <div className='h-10 w-10 xs:h-12 xs:w-12 p-1 rounded-full border border-neutral-900 opacity-0'></div>
@@ -440,7 +447,7 @@ const ChatBox = ({ room, loggedInUser, socket, scrollToTop }) => {
                     </div>
                 </div>
             )}
-            <form onSubmit={handleSubmit} className='px-1 border-t-1 border-black h-12 bg-black sm:rounded-b-xl sm:py-5 sm:px-3 sm:h-24 sm:border-neutral-900 sm:bg-neutral-900 flex items-center absolute left-0 bottom-0 w-full'>
+            <form onSubmit={handleSubmit} onKeyDown={handleKeyDown} className='px-1 border-t-1 border-black h-12 bg-black sm:rounded-b-xl sm:py-5 sm:px-3 sm:h-24 sm:border-neutral-900 sm:bg-neutral-900 flex items-center absolute left-0 bottom-0 w-full'>
                 <div className='relative cursor-pointer w-16 h-full flex items-center justify-center border-r border-neutral-900 rounded-l-lg bg-neutral-950 sm:p-3 sm:h-full sm:w-16 hover:bg-neutral-800 transition-colors ease-in-out duration-300'>
                     <IoMdAttach />
                     <input ref={fileInputRef} onChange={handleImageMessageChange} type='file' accept='image/*' className='absolute top-0 left-0 cursor-pointer opacity-0 border h-full w-full border-red-600' />
--- src/components/VideoSettingsTab.jsx
+++ src/components/VideoSettingsTab.jsx
@@ -31,13 +31,13 @@ const VideoSettingsTab = ({ room, loggedInUser, formatTime }) => {
     }
 
     const handleChangeVideo = async (video, room) => {
-        if (!video._id) return
-        if (!room._id) return
+        if (!video?._id) return
+        if (!room?._id) return
         if ((room.owner.every(owner => owner._id !== loggedInUser._id)) && (room.admins.every(admin => admin._id !== loggedInUser._id))) return
         if (isChangeRoomVideoLoading) return
-        if (video._id === room.video._id) return
+        if (video?._id === room.video?._id) return
 
-        setSelectedVideo(video._id)
+        setSelectedVideo(video?._id)
 
         const payload = {
             roomId: room._id,
@@ -59,7 +59,7 @@ const VideoSettingsTab = ({ room, loggedInUser, formatTime }) => {
     }, [searchInput])
 
     return (
-        <div className='rounded-l-2xl sm:rounded-2xl overflow-y-scroll sm:overflow-auto w-[260px] xs:w-[350px] h-[380px] sm:h-auto sm:w-[450px] md:w-[550px] -mt-10 sm:mt-16 lg:mt-0 lg:w-[600px] px-0 xs:px-2 sm:px-8 py-8 relative bg-black border border-neutral-900 text-sm'>
+        <div className='rounded-l-2xl sm:rounded-2xl overflow-y-scroll sm:overflow-auto w-[260px] xs:w-[350px] h-[300px] sm:h-auto sm:w-[450px] md:w-[550px] -mt-10 sm:mt-16 lg:mt-0 lg:w-[600px] px-0 xs:px-2 sm:px-8 py-8 relative bg-black border border-neutral-900 text-sm'>
             {Array.from({ length: 3 }, (_, i) => i + 1).map((number, index) => (
                 <div key={index} className='z-10 bg-transparent backdrop-filter backdrop-blur-2xl shadow-lg absolute w-full h-screen sm:h-full top-0 left-0 rounded-xl'></div>
             ))}
@@ -81,7 +81,7 @@ const VideoSettingsTab = ({ room, loggedInUser, formatTime }) => {
                 
                 <Separator className={'bg-neutral-800 mt-5'} />
                 
-                {(!room.video || room.video.length < 1) ? (
+                {(!room.video || room.video?.length < 1) ? (
                     <div className='py-5 flex flex-col items-center justify-center gap-4'>
                         <div className='h-[80px] w-[80px] bg-neutral-900 border border-white rounded-full flex items-center justify-center'>
                             <FaVideoSlash style={{ fontSize: '25px' }} />
@@ -91,7 +91,7 @@ const VideoSettingsTab = ({ room, loggedInUser, formatTime }) => {
                 ) : (
                     <div className='py-5 flex flex-col items-center gap-8 sm:gap-2'>
                         <div className='relative h-32 w-32'>
-                            <img className='h-full w-full rounded-xl object-cover' src={room.video.coverPhoto} alt="image" />
+                            <img className='h-full w-full rounded-xl object-cover' src={room.video?.coverPhoto} alt="image" />
                             {isChangeRoomVideoLoading && (
                                 <div className='absolute z-10 top-0 left-0 flex items-center justify-center black-opacity h-full w-full rounded-xl'>
                                     <div className='loader'></div>
@@ -101,25 +101,25 @@ const VideoSettingsTab = ({ room, loggedInUser, formatTime }) => {
                                 <AudioVisualiser />
                             </div>
                         </div>
-                        <div className='w-full text-xs xs:text-sm flex items-center justify-center xs:pl-5 sm:pl-0'>{room.video.name}</div>
+                        <div className='w-full text-xs xs:text-sm flex items-center justify-center xs:pl-5 sm:pl-0'>{room.video?.name}</div>
                     </div>
                 )}
 
                 {searchInput && (
                     <>
                         {isSearchForVideosLoading ? (
-                            <div className={`mt-6 sm:mt-24 left-0 top-10 w-full z-10 shadow-lg px-7 sm:p-0 sm:z-5 sm:w-full rounded-lg bg-black text-white absolute sm:right-0 sm:mt-2 transition-opacity duration-500 ease-in-out ${isSearchForVideosLoading} ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+                            <div className={`mt-6 left-0 top-10 w-full z-10 shadow-lg px-7 sm:p-0 sm:z-5 sm:w-full rounded-lg bg-black text-white absolute sm:right-0 sm:mt-2 transition-opacity duration-500 ease-in-out ${isSearchForVideosLoading} ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
                                 <div className='p-4 sm:p-3 border border-neutral-800 flex justify-center rounded-lg'>
                                     <div className='loader'></div>
                                 </div>
                             </div>
                         ) : (
                             <div className={`bg-transparent max-h-[200px] sm:max-h-[270px] rounded-lg left-0 top-10 w-full shadow-lg z-10 px-7 sm:p-0 sm:z-5 sm:w-full bg-black text-white absolute sm:right-0 mt-4 sm:mt-2 transition-opacity duration-500 ease-in-out ${videos ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
-                                <div className={`${videos?.length > 2 ? 'overflow-y-scroll' : 'null'} sm:${videos?.length > 3 ? 'overflow-y-scroll' : 'null'} max-h-[200px] sm:max-h-[270px] bg-black rounded-lg border w-full border-neutral-800 flex flex-col`} >
+                                <div className={`${videos?.length > 1 ? 'overflow-y-scroll' : 'null'} sm:${videos?.length > 3 ? 'overflow-y-scroll' : 'null'} max-h-[200px] sm:max-h-[270px] bg-black rounded-lg border w-full border-neutral-800 flex flex-col`} >
                                     {videos?.length > 0 ? (
                                         videos?.map((video, index) => (
                                             <div onClick={() => handleChangeVideo(video, room)} key={index} className={`relative transition-colors duration-300 ease-in-out ${isChangeRoomVideoLoading ? 'bg-neutral-900' : null} hover:bg-neutral-900  ${index === 0 && (videos?.length < 3) ? 'rounded-t-lg' : null} ${(index === videos?.length-1) && (videos?.length < 3) ? 'rounded-b-lg' : null}`}>
-                                                {video._id === room.video._id && (
+                                                {video._id === room.video?._id && (
                                                     <div className={`absolute black-opacity top-0 left-0 w-full h-full ${index === 0 && (videos?.length < 3) ? 'rounded-t-lg' : null} ${(index === videos?.length-1) && (videos?.length < 3) ? 'rounded-b-lg' : null}`}></div> 
                                                 )}
                                                 <div className={`px-4 pt-4 pb-4 ${(room.owner.every(owner => owner._id !== loggedInUser._id)) && (room.admins.every(admin => admin._id !== loggedInUser._id)) ? null :'cursor-pointer'} flex flex-col gap-2 text-sm ${index !== 0 ? null : 'rounded-t-lg'} ${index === videos.length-1 ? 'rounded-b-lg' : null}`}>
